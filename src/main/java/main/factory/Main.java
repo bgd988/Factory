ef544ba491a99fb2717ffac730dffe2fd3487f19
bgd988 <zemun88@gmail.com>
2020-02-06 20:34:28 +0100

    Prvo dodavanje

---
+++ src/main/java/main/factory/Main.java
@@ -0,0 +1,35 @@
+package main.factory;
+
+/**
+ *
+ * @author mojme
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Pocetak");
+        
+        String message = "Ovo je poruka";
+        
+        Car fiat = new Car();
+        
+        fiat.model = "Fiat Punto";
+        fiat.buildYear = 2010;
+        fiat.color = "black";
+        fiat.consumption = 6;
+        fiat.maxSpeed = 180;
+        fiat.currentFuel = 20;
+        fiat.maxFuel = 45;
+        
+        System.out.println("Model:" + fiat.model);
+        
+        Car ford = new Car();
+        ford.model = "Fiesta";
+        ford.buildYear = 2015;
+    }
+    
+}
