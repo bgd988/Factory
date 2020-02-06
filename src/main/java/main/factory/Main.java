ba0d26550214558c33d595652d88cb1fe6b7ae04
bgd988 <zemun88@gmail.com>
2020-02-06 23:46:25 +0100

    Dodata su dva nova automobila

--- src/main/java/main/factory/Main.java
+++ src/main/java/main/factory/Main.java
@@ -15,6 +15,8 @@ public class Main {
         
         String message = "Ovo je poruka";
         
+        System.out.println();
+        
         Car fiat = new Car();
         
         fiat.model = "Fiat Punto";
@@ -26,10 +28,52 @@ public class Main {
         fiat.maxFuel = 45;
         
         System.out.println("Model:" + fiat.model);
+        System.out.println("Godina proizvodnje:" + fiat.buildYear);
+        System.out.println("Boja:" + fiat.color);
+        System.out.println("Potrosnja:" + fiat.consumption);
+        System.out.println("Maksimalna brzina:" + fiat.maxSpeed);
+        System.out.println("Stanje rezervoara:" + fiat.currentFuel);
+        System.out.println("Maksimum rezervoara:" + fiat.maxFuel);
+        
+        System.out.println();
         
         Car ford = new Car();
-        ford.model = "Fiesta";
+        
+        ford.model = "Focus";
         ford.buildYear = 2015;
+        ford.color = "white";
+        ford.consumption = 10;
+        ford.maxSpeed = 220;
+        ford.currentFuel = 30;
+        ford.maxFuel = 60;
+       
+        System.out.println("Model:" + ford.model);
+        System.out.println("Godina proizvodnje:" + ford.buildYear);
+        System.out.println("Boja:" + ford.color);
+        System.out.println("Potrosnja:" + ford.consumption);
+        System.out.println("Maksimalna brzina:" + ford.maxSpeed);
+        System.out.println("Stanje rezervoara:" + ford.currentFuel);
+        System.out.println("Maksimum rezervoara:" + ford.maxFuel);
+        
+        System.out.println();
+        
+        Car bmw = new Car();
+        
+        bmw.model = "X5";
+        bmw.buildYear = 2019;
+        bmw.color = "blue";
+        bmw.consumption = 12;
+        bmw.maxSpeed = 250;
+        bmw.currentFuel = 50;
+        bmw.maxFuel = 100;
+       
+        System.out.println("Model:" + bmw.model);
+        System.out.println("Godina proizvodnje:" + bmw.buildYear);
+        System.out.println("Boja:" + bmw.color);
+        System.out.println("Potrosnja:" + bmw.consumption);
+        System.out.println("Maksimalna brzina:" + bmw.maxSpeed);
+        System.out.println("Stanje rezervoara:" + bmw.currentFuel);
+        System.out.println("Maksimum rezervoara:" + bmw.maxFuel);
     }
     
 }
